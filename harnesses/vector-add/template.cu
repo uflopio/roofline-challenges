2cba0187981a043e324b0236d0f31f97af5ab465
oxrinz <roberts.denisovs@gmail.com>
2026-05-12 14:41:50 +0300

    Scaffold vector-add harness

---
+++ harnesses/vector-add/template.cu
@@ -0,0 +1,7 @@
+#include <cuda_runtime.h>
+
+// Implement vector addition: c[i] = a[i] + b[i] for i in 0..n-1.
+// a, b, c are device pointers; you allocate kernels + pick launch config.
+void solve(const float *a, const float *b, float *c, int n) {
+
+}
