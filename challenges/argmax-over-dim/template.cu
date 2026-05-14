4f5af1a39485539f0f76dd1152486b99d29b0e0e
Artur Roos <artur.roos@ktnlvr.dev>
2026-05-15 02:42:05 +0300

    Remove the extra templates
    Committed at thinkinrocks.com

--- challenges/argmax-over-dim/template.cu
+++
@@ -1,6 +0,0 @@
-#include <cuda_runtime.h>
-
-void solve(float* input, int* shape, int shape_size, int dim, float* output) {
-  // Solution goes here.
-  // Keep in mind: the pointers are on the GPU!
-}
--- challenges/box-blur/template.cu
+++
@@ -1,6 +0,0 @@
-#include <cuda_runtime.h>
-
-void solve(const float* input_image, float* output_image, int height, int width, int kernel_size) {
-  // Solution goes here.
-  // Keep in mind: the pointers are on the GPU!
-}
