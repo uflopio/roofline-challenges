51ab8d4c16462c0f51df9288becd0882cb87facc
Artur Roos <artur.roos@ktnlvr.dev>
2026-05-16 16:58:50 +0300

    Use better challenge definitions for cryptographic problems
    Committed at thinkinrocks.com

--- challenges/hill-rekey/challenge.py
+++ challenges/hill-rekey/challenge.py
@@ -1,11 +1,27 @@
 import sympy as sp
+import numpy as np
+from math import gcd
 
 
-def solve(A: list[list[int]], B: list[list[int]], n, l: int, c: list):
+def generate(n: int, l: int) -> dict:
+    c = np.random.randint(0, l, size=n)
+    B = np.random.randint(0, l, size=(n, n))
+
+    while True:
+        A_np = np.random.randint(0, l, size=(n, n))
+        A = sp.Matrix(A_np)
+
+        det_A = int(A.det())
+
+        if gcd(det_A, l) == 1:
+            return {'A': A_np, 'B': B, 'c': c, 'l': l, 'n': n}
+
+
+def solve(A: np.ndarray, B: np.ndarray, n, l: int, c: np.ndarray):
     a = sp.Matrix(A)
     b = sp.Matrix(B)
     a_inv = a.inv_mod(l)
-    c[:] = (b * a_inv * sp.Matrix(c)) % l
+    c[:] = np.array((b * a_inv * sp.Matrix(c)) % l).reshape(-1)
 
 
 c = [7, 11]
--- challenges/matrix-inverse-modulo-n/challenge.py
+++ challenges/matrix-inverse-modulo-n/challenge.py
@@ -1,5 +1,8 @@
 import numpy as np
+import sympy as sp
+from math import gcd
 
 
 def solve(n, A: np.ndarray, m: int):
-    return np.gcd(np.linalg.det(A), m) == 1
+    det = sp.Matrix(A).det()
+    return gcd(int(det), m) == 1
